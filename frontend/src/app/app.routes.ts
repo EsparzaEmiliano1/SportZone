988c8ce5976743233f9c101474313a3e4cb670eb
Jose Alejandro Contreras Becerra <mymmagasho123@gmail.com>
2026-04-17 19:06:50 -0600

    Ver Catalogo y Soporte en pagina de inicio ya redirige

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,16 +5,14 @@ import { DetalleComponent } from './pages/detalle/detalle.component';
 import { AltaProductoComponent } from './pages/alta-producto/alta-producto.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
-import { ProductoDetalleComponent } from './pages/producto-detalle/producto-detalle.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-    { path: 'inicio', component: InicioComponent },
-    { path: 'catalogo', component: CatalogoComponent },
-    { path: 'alta-producto', component: AltaProductoComponent },
-    { path: 'productos/:id', component: DetalleComponent }, 
-    { path: 'carrito', component: CarritoComponent },
-    { path: 'contacto', component: ContactoComponent },
-    { path: 'productos/:id', component: ProductoDetalleComponent },
-    { path: '**', redirectTo: 'inicio' }  // Para rutas 404, redirige a inicio 
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'alta-producto', component: AltaProductoComponent },
+  { path: 'productos/:id', component: DetalleComponent }, 
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: 'inicio' }
 ];
